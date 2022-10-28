221a533384192954dda5b5aaa39409819dd112cd
midlynj <jmidlyn@gmail.com>
2022-10-27 19:17:52 -0500

    navigation refactored

--- src/navbar/Navigation.js
+++ src/navbar/Navigation.js
@@ -1,13 +1,28 @@
-import {Link} from "react-router-dom";
+import {Link, Route} from "react-router-dom";
+import Logout from "../component/Logout";
+import {useEffect, useState} from "react";
 
 const Navigation = () => {
+
+    let checkIfLogin = localStorage.getItem("authenticated")
 
     return (
         <nav className= "navbar">
             <div  className= "links">
                 <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-                <Link to="/register">Register</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-                <Link to="/login">Login</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+
+                {!checkIfLogin ? (
+                    <>
+                        <Link to="/register">Register</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to="/login">Login</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                   </>
+
+                    )
+
+: (                 <>
+                            <Link to="/logout">logout</Link>
+                    </>
+                )}
 
             </div>
         </nav>
