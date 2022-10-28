3cb00804a5e729a82ca5fec0299de3bb34d36325
midlynj <jmidlyn@gmail.com>
2022-10-28 18:04:24 -0500

    home component return a greeting depending on if user is logged in

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,22 @@
+import UserGreeting from "./UserGreeting";
+import GuestGreeting from "./GuestGreeting";
+
 const Home = () => {
+localStorage.getItem("authenticated")
+    let checkForLogin = localStorage.getItem("authenticated")
 
-    return (
-
-        <div className="home">
-            Home screen
-        </div>
-    )
+    if (checkForLogin) {
+        return <UserGreeting/>
+    }  return <GuestGreeting/>
+    // return (
+    //
+    //     <div className="home">
+    //         Home screen
+    //
+    //         <p>{"Hello " +m}</p>
+    //     </div>
+    // )
 
 }
+
 export default Home
