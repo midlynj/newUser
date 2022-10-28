eecc73800bf47eaca4ea088967f60b8f127d7f6d
midlynj <jmidlyn@gmail.com>
2022-10-28 17:59:05 -0500

    login component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navigation from "./navbar/Navigation";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Register from "./component/Register";
+import Home from "./component/Home";
+import Login from "./component/Login";
+import Logout from "./component/Logout";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [authenticated, setAuthenticated] = useState(localStorage.getItem(localStorage.getItem("authenticated")|| false));
+
+
+    return (
+
+      <Router>
+            <div className="App">
+
+              <Navigation/>
+
+                <div className = "content">
+
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+
+                        <Route path="/register" element={<Register/>}/>
+
+                        <Route path="/login" element={<Login/>}/>
+
+                        <Route path="/logout" element={<Logout/>}/>
+
+
+
+                    </Routes>
+
+                </div>
+            </div>
+      </Router>
+
   );
 }
 
