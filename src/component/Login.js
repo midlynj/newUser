10136c3a083714e2b3124f7a834a084d4d830f1d
midlynj <jmidlyn@gmail.com>
2022-10-29 20:43:13 -0500

    css for login

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import axios from "axios";
+import "./login.css"
 
 const Login = () => {
     const [username, setUsername] = useState("");
@@ -30,25 +31,56 @@ const Login = () => {
 
     return (
 
-        <div>
-            <p>Welcome Back</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="Username"
-                    placeholder="username"
+        <div className="login">
 
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    name="Password"
-                    placeholder="password"
+            <div className="login-box">
+                <h2>Welcome back</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="user-box">
+                        <input
+                            type="text"
+                            name="Username"
+                            required
 
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <input type="submit" value="Submit" />
-            </form>
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <label>Username</label>
+
+
+                    </div>
+                    <div className="user-box">
+                        <input
+                            type="password"
+                            name="Password"
+                            required
+
+                            onChange={(e) => setPassword(e.target.value)}
+
+                        />
+                        <label>Password</label>
+
+                    </div>
+                    <input type="submit" value="Submit" className="login-button"/>
+                </form>
+            </div>
+            {/*<p>Welcome Back</p>*/}
+            {/*<form onSubmit={handleSubmit}>*/}
+                {/*<input*/}
+                {/*    type="text"*/}
+                {/*    name="Username"*/}
+                {/*    placeholder="username"*/}
+
+                {/*    onChange={(e) => setUsername(e.target.value)}*/}
+                {/*/>*/}
+                {/*<input*/}
+                {/*    type="password"*/}
+                {/*    name="Password"*/}
+                {/*    placeholder="password"*/}
+
+                {/*    onChange={(e) => setPassword(e.target.value)}*/}
+                {/*/>*/}
+                {/*<input type="submit" value="Submit" className="login-button"/>*/}
+            {/*</form>*/}
         </div>
 
 
