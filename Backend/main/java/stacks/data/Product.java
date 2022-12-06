8b79652b916becf85ce073bddf5910e55967a97b
midlynj <jmidlyn@gmail.com>
2022-12-06 10:05:55 -0600

    link backend to database

--- Backend/main/java/stacks/data/Product.java
+++ Backend/main/java/stacks/data/Product.java
@@ -2,15 +2,22 @@ package stacks.data;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "users")
 
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, length = 100, unique = true)
     private String name;
     private long quantity;
     private long price;
