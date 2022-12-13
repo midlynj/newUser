929da925374f82deb378861e7187e2408ff66afc
midlynj <jmidlyn@gmail.com>
2022-12-13 14:31:29 -0600

    link users to database

--- Backend/main/java/stacks/data/User.java
+++ Backend/main/java/stacks/data/User.java
@@ -1,17 +1,35 @@
 package stacks.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+
+import javax.persistence.*;
+import java.util.Collection;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "users")
 
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, length = 100, unique = true)
     private String username;
+
+    @Column(nullable = false, length = 100)
     private String email;
+
+    @Column(nullable = false, length = 100)
     private String password;
+
+    @OneToMany(mappedBy = "author")
+    @JsonIgnoreProperties("author")
+    private Collection<Product> itemsBought;
+
 }
